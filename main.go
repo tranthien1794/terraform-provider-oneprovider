3d45d915dba10caa79e22ee669bdf401c5637935
thienlytran <thienlytran@192.168.1.219.non-exists.ptr.local>
2025-03-31 16:39:41 +0700

    init

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
     "context" // Added missing import
     "github.com/hashicorp/terraform-plugin-framework/provider"
     "github.com/hashicorp/terraform-plugin-framework/providerserver"
-    "github.com/yourusername/terraform-provider-oneprovider/oneprovider"
+    "https://github.com/tranthien1794/terraform-provider-oneprovider"
 )
 
 func main() {
     providerserver.Serve(context.Background(), func() provider.Provider {
         return oneprovider.New()
     }, providerserver.ServeOpts{
-        Address: "registry.terraform.io/yourusername/oneprovider",
+        Address: "registry.terraform.io/tranthien1794/oneprovider",
     })
 }
